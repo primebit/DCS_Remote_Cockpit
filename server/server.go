1b4a5b764679f2b4d1204dc695bde61376bc9cba
primebit <redlinesoft@yandex.ru>
2015-05-16 19:46:30 +0300

    First stable version

---
+++ server/server.go
@@ -0,0 +1,73 @@
+package server
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"code.google.com/p/go.net/websocket"
+	"log"
+	"html/template"
+)
+
+var messageQueues []chan string
+
+func Serve(messageChan chan string) {
+	go func(messageQueues *[]chan string) {
+		for message := range messageChan {
+			go cloneMessageToQueues(message, messageQueues)
+		}
+	}(&messageQueues)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", IndexPage)
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	http.Handle("/", r)
+	http.Handle("/socket", websocket.Handler(DataSocket))
+	http.ListenAndServe(":8100", nil)
+}
+
+func IndexPage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./views/index.html")
+	if err != nil {
+		log.Println("Index page:", err)
+	}
+	t.Execute(w, "")
+}
+
+func cloneMessageToQueues(message string, messageQueues *[]chan string) {
+	for _,queue := range *messageQueues {
+		if queue != nil {
+			queue <- message
+		}
+	}
+}
+
+func DataSocket(ws *websocket.Conn) {
+	queue := make(chan string)
+
+	messageQueues = append(messageQueues, queue)
+	queueId := len(messageQueues)-1
+
+	log.Println("New websocket connection #", queueId)
+
+	for message := range queue {
+		_, err := ws.Write([]byte("{\"type\":\"update\", \"data\":" + message + "}"))
+		if err != nil {
+			deleteQueue(queueId)
+			log.Println("Websocket transmit error, connection closed #", queueId)
+			break
+		} else {
+			response := make([]byte, 10)
+			n, err := ws.Read(response)
+			if err != nil || n != 2 {
+				deleteQueue(queueId)
+				log.Println("Websocket confirm receive error, connection closed #", queueId)
+				break
+			}
+		}
+	}
+	ws.Close()
+}
+
+func deleteQueue(queueId int) {
+	messageQueues[queueId] = nil
+}
